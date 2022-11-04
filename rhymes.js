757264fe1a67e4ff995b201aaa28074573a0b9c5
amoh1480 <amohammadali1480@gmail.com>
2022-11-04 13:31:21 -0400

    Current progress from in class.
    Co-authored-by: alyssa-sharp <alyssa-sharp@users.noreply.github.com>
    Co-authored-by: Dean Ludwig <amosdean@users.noreply.github.com>

---
+++ rhymes.js
@@ -0,0 +1,21 @@
+// const wordRhyme = document.getElementById("word");
+// console.log(wordRhyme);
+
+let wordArray =  "https://rhymebrain.com/talk?function=getRhymes&word=hello"
+
+
+for (let i = 0; i < 50; i++) {
+    
+    console.log(wordArray)
+}
+let objectArray = document.getElementsByClassName("RhymeBrainResult");
+
+console.log(objectArray)
+
+// objectArray.map((element) => {
+//     // element.
+// })
+
+
+console.log();
+
